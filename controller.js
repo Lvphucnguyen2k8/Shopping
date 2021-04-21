70795c51546ea6a02a00a9cab70d4057193e080b
mongchuyen <mongchuyen0102@gmail.com>
2021-04-21 23:20:20 +0700

    chuyenvm:load products

--- controller.js
+++ controller.js
@@ -3,12 +3,45 @@ window.onload = function() {
     getProducts()
 }
 
-
+// function addProduct() {
+//     console.log("ccgtguj")
+//     var node = document.createElement("div")
+//     var add = document.createTextNode("Chuyencute");
+//     node.appendChild(add);
+//     document.getElementById("myList").append(node)
+// }
+// function addTest() {
+//     var myTable = document.getElementById("test");
+//     var myTd = document.createElement("td");
+//     myTable.appendChild(myTd);
+// }
+function test() {
+    var a = document.getElementById("tr");
+    console.log(a)
+}
 function getProducts() {
     fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
-            .then(json=> console.log(json))
+            .then(json=> {
+                var list = document.getElementById("tr");
+                let str='';
+                json.forEach(element => {
+                    str+=`<tr>
+                        <td>${element.id}</td>
+                        <td>${element.category}</td>
+                        <td>${element.description}</td>
+                        <td>${element.image}</td>
+                        <td>${element.price}</td>
+                        <td>${element.title}</td>
+                    </tr>`
+                });
+                list.outerHTML=str;
+                console.log(json)
+                // console.log(json[0])
+            })
 }
+
+
 
 function getProduct(id){
 
