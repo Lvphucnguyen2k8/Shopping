2f59b481afbcd53ba97e19e7a6645cec55d1b423
Lvphucnguyen2k8 <70436761+Lvphucnguyen2k8@users.noreply.github.com>
2021-07-31 11:17:22 +0700

    .

--- controller/controller.js
+++
@@ -1,9 +0,0 @@
-//import {initHtml} from './menu.js';
-import {initProducts} from './db.js'
-//import { product } from './models/product.js';
-
-window.onload = () => {
-    //initHtml()
-    initProducts()
-}
-
